004677b1a72648ecadd3ddbed34e8ced9272b029
tmartinez <informatique@market-pie.com>
2015-05-20 21:54:07 +0200

    encode device name in utf-8

--- ressources/zwaveserver/rest-server.py
+++ ressources/zwaveserver/rest-server.py
@@ -993,6 +993,7 @@ def setDeviceName(device_id, name) :
     debugPrint("setName for device_id:%s New Name ; '%s'" % (device_id, name,))
     result = False
     if(device_id in network.nodes) : 
+        name = name.encode('utf8')
         name = name.replace('+',' ')
         network.nodes[device_id].set_field('name',name)
         result = True
