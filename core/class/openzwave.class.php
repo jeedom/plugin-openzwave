e78eb9f382a9c534586b0896faf07219857aa0a6
Loic <loicgevrey@gmail.com>
2015-06-14 21:54:41 +0200

    Fix openzawe stop

--- core/class/openzwave.class.php
+++ core/class/openzwave.class.php
@@ -445,14 +445,7 @@ class openzwave extends eqLogic {
 
 	public static function stopDeamon() {
 		self::callOpenzwave('/ZWaveAPI/network_stop()');
-		$i = 0;
-		while (self::deamonRunning()) {
-			sleep(1);
-			$i++;
-			if ($i > 5) {
-				break;
-			}
-		}
+		sleep(5);
 		$pid_file = '/tmp/openzwave.pid';
 		if (file_exists($pid_file)) {
 			$pid = intval(trim(file_get_contents($pid_file)));
