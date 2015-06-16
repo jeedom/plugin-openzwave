92b767d2fcdf268006e0327309cb3c74080ccd40
tmartinez69009 <tmartinez69009@gmail.com>
2015-06-16 15:27:51 +0200

    Change config directory and add config files into the plugin

--- ressources/zwaveserver/openZWave.py
+++ ressources/zwaveserver/openZWave.py
@@ -437,7 +437,7 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGTERM, signal_handler)
 
 #Define some manager options
-options = ZWaveOption(device, config_path="/opt/python-openzwave/openzwave/config", user_path="/opt/python-openzwave/", cmd_line="")
+options = ZWaveOption(device, config_path="/usr/share/nginx/www/jeedom/plugins/openzwave/ressources/openzwave/config", user_path="/opt/python-openzwave/", cmd_line="")
 options.set_log_file("openzwave.log")
 options.set_append_log_file(False)
 options.set_console_output(False)
