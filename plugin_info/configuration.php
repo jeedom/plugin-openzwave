7fb020e1faa44edb7d947140e1c89f8e834c0169
Loic <loicgevrey@gmail.com>
2015-06-20 13:53:19 +0200

    Fix link

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -112,7 +112,7 @@ if (jeedom::isCapable('sudo')) {
 				</div>
 			</div>';
 } else {
-	echo '<div class="alert alert danger">{{Jeedom n\'a pas les droits sudo sur votre système, il faut lui ajouter pour qu\'il puisse installer le démon openzwave, voir <a target="_blank" href="http://doc.jeedom.fr/fr_FR/doc-installation.html#autre">ici</a> partie 1.7.4}}</div>';
+	echo '<div class="alert alert danger">{{Jeedom n\'a pas les droits sudo sur votre système, il faut lui ajouter pour qu\'il puisse installer le démon openzwave, voir <a target="_blank" href="https://jeedom.fr/doc/documentation/installation/fr_FR/doc-installation.html#autre">ici</a> partie 1.7.4}}</div>';
 }
 ?>
 	<div class="form-group">
