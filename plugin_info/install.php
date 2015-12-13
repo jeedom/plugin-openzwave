0ba2c72beb8a416d64c2b560c2a0cba8cf6b52f6
Loïc <loic@jeedom.com>
2015-12-13 10:09:01 +0100

    Add pause time

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -33,6 +33,7 @@ function openzwave_update() {
 	if (count(eqLogic::byType('zwave')) > 0) {
 		log::add('openzwave', 'error', 'Attention vous etes sur la nouvelle version d\'openzwave, des actions de votre part sont necessaire merci d\'aller voir https://jeedom.fr/blog/?p=1576');
 	}
+	sleep(2);
 	if (openzwave::deamonRunning()) {
 		openzwave::runDeamon();
 	}
