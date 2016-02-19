2d52fcdb2b080d2a39e239f357c34508a8bfb301
loic <lgevrey@oxya.com>
2016-02-19 13:37:48 +0100

    Code cleaning

--- desktop/js/health.js
+++ desktop/js/health.js
@@ -96,12 +96,10 @@ var app_health = {
       if(nodes[i].data == undefined){
         continue;
       }
-      if(nodes[i].data.description.name != ''){
-        if(isset(eqLogic_human_name[$('#sel_zwaveHealthServerId').value()+':'+i])){
-          var name = '<span class="nodeConfiguration cursor" data-node-id="'+i+'" data-server-id="'+$("#sel_zwaveHealthServerId").value()+'">'+eqLogic_human_name[$('#sel_zwaveHealthServerId').value()+':'+i]+'</span>';
-        }else{
-          var name = '<span class="nodeConfiguration cursor" data-node-id="'+i+'" data-server-id="'+$("#sel_zwaveHealthServerId").value()+'"><span  class="label label-primary" style="font-size : 1em;">'+nodes[i].data.description.location+'</span> '+nodes[i].data.description.name+'</span>';
-        }
+      if(isset(eqLogic_human_name[$('#sel_zwaveHealthServerId').value()+':'+i])){
+        var name = '<span class="nodeConfiguration cursor" data-node-id="'+i+'" data-server-id="'+$("#sel_zwaveHealthServerId").value()+'">'+eqLogic_human_name[$('#sel_zwaveHealthServerId').value()+':'+i]+'</span>';
+      }else if(nodes[i].data.description.name != ''){
+        var name = '<span class="nodeConfiguration cursor" data-node-id="'+i+'" data-server-id="'+$("#sel_zwaveHealthServerId").value()+'"><span  class="label label-primary" style="font-size : 1em;">'+nodes[i].data.description.location+'</span> '+nodes[i].data.description.name+'</span>';
       }else{
         var name = '<span class="nodeConfiguration cursor" data-node-id="'+i+'" data-server-id="'+$("#sel_zwaveHealthServerId").value()+'">'+ nodes[i].data.description.product_name+'</span>';
       }
