942d2666c3e11473478474954feaa1c23e143e05
Loïc <loic@jeedom.com>
2016-05-20 14:25:57 +0200

    readd conf

--- resources/syncconf.sh
+++ resources/syncconf.sh
@@ -18,14 +18,14 @@ fi
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ -d  ${BASEDIR}/../core/config/devices ]; then
 	echo "Suppression des configurations Jeedom existantes"
-	sudo rm -fr ${BASEDIR}/../core/config/devices/*
+	sudo rm -rf ${BASEDIR}/../core/config/devices/*
 	echo "Recopie des nouvelles configurations Jeedom"
 	cd plugin-openzwave/core/config/devices
 	sudo mv * ${BASEDIR}/../core/config/devices/
 	echo "Suppression des configurations Openzwave existantes"
-	sudo rm -fr ${BASEDIR}/../resources/openzwaved/config/*
+	sudo rm -rf ${BASEDIR}/../resources/openzwaved/config/*
 	echo "Recopie des nouvelles configurations Openzwave"
-	cd /tmp/plugin-openzwave/resources/openzwaved/config
+	cd /tmp/plugin-openzwave/resources/openzwave/config
 	sudo mv * ${BASEDIR}/../resources/openzwaved/config/
 	echo "Nettoyage du répertoire temporaire"
 	sudo rm -R /tmp/plugin-openzwave
