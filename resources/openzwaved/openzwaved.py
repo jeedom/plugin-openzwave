03a904c2cc4a7b4054d130ec7281e377c386fefa
Loïc <loic@jeedom.com>
2016-05-25 18:14:36 +0200

    Prevent issue if log level is notice

--- resources/openzwaved/openzwaved.py
+++ resources/openzwaved/openzwaved.py
@@ -717,7 +717,11 @@ options = ZWaveOption(_device, config_path=_config_folder, user_path=_data_folde
 options.set_log_file("../../../log/openzwaved")
 options.set_append_log_file(False)
 options.set_console_output(False)
-options.set_save_log_level(_log_level[0].upper() + _log_level[1:])
+if _log_level = 'notice':
+    options.set_save_log_level('Error')
+else:
+    options.set_save_log_level(_log_level[0].upper() + _log_level[1:])
+    
 options.set_logging(True)
 options.set_associate(True)                       
 options.set_save_configuration(True)              
