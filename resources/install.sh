b6cb87b6d38023f5c9b23289bfd7042070988e74
Nechry <nechry@gmail.com>
2016-06-19 10:04:58 +0200

    update ozw lib to 1.4.296
    remove dependancy check

--- resources/install.sh
+++ resources/install.sh
@@ -12,7 +12,7 @@ echo "Lancement de l'installation/mise à jour des dépendances openzwave"
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ARCH=`uname -m`
 PYTHON_OPENZWAVE_VERSION=1ca17acabc64fe857f7c1ee412426af8defaaaf0 # 0.3.0
-OPENZWAVE_VERSION=2e25b5d9a5783e0bf2d380e18c134dc5ec767191 # 1.4.248
+OPENZWAVE_VERSION=ad17189ba40349b547e601541a0f8307253799c9 # 1.4.296
 
 function apt_install {
   sudo apt-get -y install "$@"
