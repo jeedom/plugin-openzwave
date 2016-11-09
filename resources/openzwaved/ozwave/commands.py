97ca0e6a5af064c8769ef25b277ee1d7f7a4882e
Loïc <loic@jeedom.com>
2016-11-09 11:18:59 +0100

    reduce action on send command

--- resources/openzwaved/ozwave/commands.py
+++ resources/openzwaved/ozwave/commands.py
@@ -10,37 +10,13 @@ def send_command_zwave(_node_id, _cc_id, _instance_id, _index, _value):
 	utils.check_node_exist(_node_id)
 	if _cc_id == COMMAND_CLASS_NO_OPERATION:
 		return node_utils.test_node(_node_id, 1)
-	if _cc_id == COMMAND_CLASS_WAKE_UP:
-		arr = _value.split(",")
-		wake_up_time = int(arr[0])
-		for value_id in globals.network.nodes[_node_id].get_values(class_id=COMMAND_CLASS_WAKE_UP): 
-			if globals.network.nodes[_node_id].values[value_id].label == "Wake-up Interval":
-				globals.network.nodes[_node_id].values[value_id].data = int(wake_up_time)
+	if _cc_id == COMMAND_CLASS_ASSOCIATION or _cc_id == COMMAND_CLASS_WAKE_UP:
 		return
-	if _cc_id == COMMAND_CLASS_ASSOCIATION:
-		arr = _value.split(",")
-		group = int(arr[0])
-		target_node = int(arr[1])
-		try:
-			return node_utils.assoc_action(_node_id, group, target_node,0,'add')
-		except ValueError:
-			raise Exception('Node is not Ready for associations')
 	for value_id in globals.network.nodes[_node_id].get_values(class_id=_cc_id, genre='All', type='All', readonly=False, writeonly='All'):
 		if globals.network.nodes[_node_id].values[value_id].instance - 1 == _instance_id and (_index is None or globals.network.nodes[_node_id].values[value_id].index == _index):
 			value = globals.network.nodes[_node_id].values[value_id].check_data(_value)
-			if _cc_id == COMMAND_CLASS_SWITCH_MULTILEVEL and value > 99:
-				logging.debug("Switch light ON to dim level that was last known")
-				value = 255
 			globals.network.nodes[_node_id].values[value_id].data = value
 			if globals.network.nodes[_node_id].values[value_id].genre == 'System':
 				value_utils.mark_pending_change(globals.network.nodes[_node_id].values[value_id], value)
-			if _cc_id == COMMAND_CLASS_SWITCH_MULTILEVEL and value <= 99:
-				value_utils.prepare_refresh(_node_id, value_id, value, utils.is_motor(_node_id))
-			if _cc_id == COMMAND_CLASS_THERMOSTAT_SETPOINT:
-				logging.debug("COMMAND_CLASS_THERMOSTAT_SETPOINT")
-				node_utils.save_node_value_event(_node_id,  COMMAND_CLASS_THERMOSTAT_SETPOINT, _index, value, _instance_id + 10)
-			if _cc_id == COMMAND_CLASS_SWITCH_BINARY:
-				worker = threading.Timer(interval=0.5, function=value_utils.refresh_switch_binary, args=(_node_id, value_id, value))
-				worker.start()
 			return True
 	raise Exception('Value not found')
