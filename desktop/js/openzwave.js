1e175e8a0258007ac87989ae21fbf1a7e134ffe2
Ludovic <ludovic@jeedom.com>
2016-11-18 11:24:11 +0100

    default to 1 here too

--- desktop/js/openzwave.js
+++ desktop/js/openzwave.js
@@ -398,7 +398,7 @@ function addCmdToTable(_cmd) {
     tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>';
     tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
     tr += '</td>';
-    tr += '<td class="expertModeVisible"><input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="instance"></td>';
+    tr += '<td class="expertModeVisible"><input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="instance" value="1"></td>';
     tr += '<td class="expertModeVisible"><input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="class" ></td>';
     tr += '<td class="expertModeVisible"><input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="index" ></td>';
     tr += '<td class="expertModeVisible">';
