7dcd0582d430f036cfca82a36f5bed4d135aca55
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-09 18:36:40 +0200

    continue migration

--- core/config/devices/benext_138/138.7.257_minikeypad.php
+++ core/config/devices/benext_138/138.7.257_minikeypad.php
@@ -2,10 +2,10 @@
 require_once dirname(__FILE__) . "/../../../../../../core/php/core.inc.php";
 include_file('core', 'authentification', 'php');
 if (!isConnect()) {
-	echo '<div class="alert alert-danger div_alert">';
-	echo translate::exec('401 - Accès non autorisé');
-	echo '</div>';
-	die();
+    echo '<div class="alert alert-danger div_alert">';
+    echo translate::exec('401 - Accès non autorisé');
+    echo '</div>';
+    die();
 }
 ?>
 <legend>Mémoires du clavier <a class="btn btn-primary btn-xs pull-right" id="bt_refreshZipatoAssist"><i class="fa fa-refresh"></i></a></legend>
@@ -36,40 +36,40 @@ if (!isConnect()) {
     <tbody>
     <tr>
         <?php
-$data = openzwave::callOpenzwave('/ZWaveAPI/Run/devices[' . init('logical_id') . ']', init('serverId'));
-$data = $data['instances'][0]['commandClasses'][99]['data'];
-for ($i = 1; $i < 11; $i++) {
-	echo '<td>';
-	echo '<a class="btn btn-success pull-right btn-xs bt_ziptatoKeypadSaveNewCode" data-position="' . $i . '"><i class="fa fa-floppy-o"></i></a>';
-	if (isset($data[$i]) && $data[$i]['val'] != '00000000000000000000') {
-		echo '<i class="fa fa-check"></i>';
-		echo '<a class="btn btn-danger pull-right btn-xs bt_ziptatoKeypadRemoveCode"  data-position="' . $i . '"><i class="fa fa-times"></i></a>';
-	} else {
-		echo '<i class="fa fa-times"></i>';
-	}
-	echo '</td>';
-}
-?>
+        $data = openzwave::callOpenzwave('/ZWaveAPI/Run/devices[' . init('logical_id') . ']', init('serverId'));
+        $data = $data['instances'][0]['commandClasses'][99]['data'];
+        for ($i = 1; $i < 11; $i++) {
+            echo '<td>';
+            echo '<a class="btn btn-success pull-right btn-xs bt_ziptatoKeypadSaveNewCode" data-position="' . $i . '"><i class="fa fa-floppy-o"></i></a>';
+            if (isset($data[$i]) && $data[$i]['val'] != '00000000000000000000') {
+                echo '<i class="fa fa-check"></i>';
+                echo '<a class="btn btn-danger pull-right btn-xs bt_ziptatoKeypadRemoveCode"  data-position="' . $i . '"><i class="fa fa-times"></i></a>';
+            } else {
+                echo '<i class="fa fa-times"></i>';
+            }
+            echo '</td>';
+        }
+        ?>
     </tr>
     </tbody>
 </table>
 
 <script>
     $('#bt_refreshZipatoAssist').on('click',function(){
-        $('#md_modal').load('index.php?v=d&plugin=openzwave&modal=device.assistant&id=<?php echo init('id'); ?>&serverId=<?php echo init('serverId'); ?>&logical_id=<?php echo init('logical_id'); ?>');
+        $('#md_modal').load('index.php?v=d&plugin=openzwave&modal=device.assistant&id=<?php echo init('id');?>&serverId=<?php echo init('serverId');?>&logical_id=<?php echo init('logical_id');?>');
     });
 
     $('.bt_ziptatoKeypadRemoveCode').on('click',function(){
         var position = $(this).attr('data-position');
         bootbox.confirm('Etes vous sur de vouloir supprimer ce code ?', function (result) {
-            var call = '/ZWaveAPI/Run/devices[<?php echo init('logical_id'); ?>].UserCode.SetRaw('+position+',[00000000000000000000],1)';
+            var call = '/ZWaveAPI/Run/devices[<?php echo init('logical_id');?>].UserCode.SetRaw('+position+',[00000000000000000000],1)';
             if (result) {
                 $.ajax({// fonction permettant de faire de l'ajax
                     type: "POST", // méthode de transmission des données au fichier php
                     url: "plugins/openzwave/core/ajax/openzwave.ajax.php", // url du fichier php
                     data: {
                         action: "callRazberry",
-                        serverId : <?php echo init('serverId'); ?>,
+                        serverId : <?php echo init('serverId');?>,
                         call: call,
                     },
                     dataType: 'json',
@@ -82,7 +82,7 @@ for ($i = 1; $i < 11; $i++) {
                             return;
                         }
                         $('#div_configureDeviceAlert').showAlert({message: 'Code enregistré, merci de réveiller votre clavier pour qu\'il soit pris en compte (touche HOME puis 1 puis Enter) ', level: 'success'});
-                        $('#md_modal').load('index.php?v=d&plugin=openzwave&modal=device.assistant&id=<?php echo init('id'); ?>&serverId=<?php echo init('serverId'); ?>&logical_id=<?php echo init('logical_id'); ?>');
+                        $('#md_modal').load('index.php?v=d&plugin=openzwave&modal=device.assistant&id=<?php echo init('id');?>&serverId=<?php echo init('serverId');?>&logical_id=<?php echo init('logical_id');?>');
                     }
                 });
             }
@@ -98,8 +98,8 @@ for ($i = 1; $i < 11; $i++) {
                 url: "plugins/openzwave/core/ajax/openzwave.ajax.php", // url du fichier php
                 data: {
                     action: "callRazberry",
-                    serverId : <?php echo init('serverId'); ?>,
-                    call: '/ZWaveAPI/Run/devices[<?php echo init('logical_id'); ?>]',
+                    serverId : <?php echo init('serverId');?>,
+                    call: '/ZWaveAPI/Run/devices[<?php echo init('logical_id');?>]',
                 },
                 dataType: 'json',
                 error: function (request, status, error) {
@@ -110,14 +110,14 @@ for ($i = 1; $i < 11; $i++) {
                         $('#div_configureDeviceAlert').showAlert({message: data.result, level: 'danger'});
                         return;
                     }
-                    var code = data.result.instances[1].commandClasses[99].data[0].val;
+                    var code = data.result.instances[0].commandClasses[99].data[0].val;
                     var call = '/ZWaveAPI/Run/devices['+configureDeviceLogicalId+'].UserCode.SetRaw('+position+',['+code+'],1)';
                     $.ajax({// fonction permettant de faire de l'ajax
                         type: "POST", // méthode de transmission des données au fichier php
                         url: "plugins/openzwave/core/ajax/openzwave.ajax.php", // url du fichier php
                         data: {
                             action: "callRazberry",
-                            serverId : <?php echo init('serverId'); ?>,
+                            serverId : <?php echo init('serverId');?>,
                             call: call,
                         },
                         dataType: 'json',
@@ -130,7 +130,7 @@ for ($i = 1; $i < 11; $i++) {
                                 return;
                             }
                             $('#div_configureDeviceAlert').showAlert({message: 'Code enregistré, merci de réveiller votre clavier pour qu\'il soit pris en compte (touche HOME puis 1 puis Enter) ', level: 'success'});
-                            $('#md_modal').load('index.php?v=d&plugin=openzwave&modal=device.assistant&id=<?php echo init('id'); ?>&serverId=<?php echo init('serverId'); ?>&logical_id=<?php echo init('logical_id'); ?>');
+                            $('#md_modal').load('index.php?v=d&plugin=openzwave&modal=device.assistant&id=<?php echo init('id');?>&serverId=<?php echo init('serverId');?>&logical_id=<?php echo init('logical_id');?>');
                         }
                     });
                 }
