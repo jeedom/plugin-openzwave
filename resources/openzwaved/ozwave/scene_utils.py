8f4c7f621b896a41d776c505b9fd1aebb80f070c
Jean-François Auger <nechry@gmail.com>
2017-06-12 08:22:23 +0200

    Use Instance 1
    We now use instance in base 1, json are in base 1 to.
    Remove Central Scene notification to avoid shadow others events

--- resources/openzwaved/ozwave/scene_utils.py
+++ resources/openzwaved/ozwave/scene_utils.py
@@ -3,5 +3,4 @@ import node_utils,globals
 
 def scene_event(network, node, scene_id):
 	logging.info('Scene Activation: %s' % (scene_id,))
-	node_utils.save_node_value_event(node.node_id,  globals.COMMAND_CLASS_CENTRAL_SCENE, 0, scene_id, 0)
-	node_utils.save_node_value_event(node.node_id,  globals.COMMAND_CLASS_SCENE_ACTIVATION, 0, scene_id, 0)
+	node_utils.save_node_value_event(node.node_id,  globals.COMMAND_CLASS_SCENE_ACTIVATION, 0, scene_id, 1)
