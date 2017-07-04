897a6dd4c4d8e84d75b7eeb4836d1abdd507f1f1
Nechry <nechry@gmail.com>
2017-07-04 21:57:11 +0200

    fix pending changes
    compare same data type

--- resources/openzwaved/ozwave/value_utils.py
+++ resources/openzwaved/ozwave/value_utils.py
@@ -137,7 +137,7 @@ def set_config(_node_id, _index_id, _value, _size):
 				result = globals.network.manager.setValue(value_id, value)
 				mark_pending_change(my_value, value,wake_up_time)
 			elif my_value.type == 'Bool':
-				value = globals.network.nodes[_node_id].values[value_id].check_data(value)
+				value = my_value.check_data(value)
 				result = globals.network.manager.setValue(value_id, value)
 				mark_pending_change(my_value, value,wake_up_time)
 			else:
@@ -148,7 +148,8 @@ def set_config(_node_id, _index_id, _value, _size):
 				result = globals.network.nodes[_node_id].set_config_param(_index_id, int(value), _size)
 				# don't mark list item if set with value
 				if my_value is not None and my_value.type != 'List':
-					mark_pending_change(my_value, value,wake_up_time)
+					value = my_value.check_data(value)
+					mark_pending_change(my_value, value, wake_up_time)
 			logging.debug('set_configuration result: %s' % (result,))
 			return
 
