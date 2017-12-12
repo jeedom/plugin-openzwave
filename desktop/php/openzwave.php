3bff046bd3cb7cf8b8f1deeb13bb53551066d81c
Loïc <loic@jeedom.com>
2017-12-12 17:56:29 +0100

    add search bar

--- desktop/php/openzwave.php
+++ desktop/php/openzwave.php
@@ -188,6 +188,7 @@ if ($controllerMode == 5) {
     </div>
 </div>
 <legend><i class="fa fa-table"></i> {{Mes équipements Z-Wave}}</legend>
+<input class="form-control" placeholder="{{Rechercher}}" style="margin-bottom:4px;" id="in_searchEqlogic" />
 <div class="eqLogicThumbnailContainer">
     <?php
 foreach ($eqLogics as $eqLogic) {
@@ -200,7 +201,7 @@ foreach ($eqLogics as $eqLogic) {
 		echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
 	}
 	echo "</center>";
-	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+	echo '<span class="name" style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
 	echo '</div>';
 }
 ?>
